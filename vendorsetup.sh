92ec81a72b7dd906a6d387c519817113492f5e55
speck99 <hector_speed@hotmail.com>
2018-11-30 00:55:40 +0000

    more derps xd

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
 cd system/core
 git reset --hard && git clean -f -d
-patch -p1 < ../../mediatek/mt6737-common/patches/Gatekeeperd-Add-use-software-gatekeeper-option.patch
+patch -p1 < ../../device/mediatek/mt6737-common/patches/Gatekeeperd-Add-use-software-gatekeeper-option.patch
 cd ../..
 cd hardware/interfaces
 git reset --hard && git clean -f -d
-patch -p1 < ../../device/mediatek/mt6737-common/patches/patches/HIDL-sensors-fallback-to-API_1_0.patch
+patch -p1 < ../../device/mediatek/mt6737-common/patches/HIDL-sensors-fallback-to-API_1_0.patch
 patch -p1 < ../../device/mediatek/mt6737-common/patches/Error-message-suppress-option.patch
 patch -p1 < ../../device/mediatek/mt6737-common/patches/Workaround-mtk-camera-blob.patch
 patch -p1 < ../../device/mediatek/mt6737-common/patches/Disable-vndk-for-the-time-being.patch
