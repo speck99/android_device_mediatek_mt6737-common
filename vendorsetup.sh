0482635d56ad473dc7083c4f8659ef89c6941987
speck99 <hector_speed@hotmail.com>
2018-11-30 00:47:40 -0800

    derp

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,8 +4,8 @@ patch -p1 < ../../device/motorola/nicklaus/patches/Gatekeeperd-Add-use-software-
 cd ../..
 cd hardware/interfaces
 git reset --hard && git clean -f -d
-patch -p1 < ../../device/motorola/nicklaus/patches/HIDL-sensors-fallback-to-API_1_0.patch
-patch -p1 < ../../device/motorola/nicklaus/patches/Error-message-suppress-option.patch
-patch -p1 < ../../device/motorola/nicklaus/patches/Workaround-mtk-camera-blob.patch
-patch -p1 < ../../device/motorola/nicklaus/patches/Disable-vndk-for-the-time-being.patch
+patch -p1 < ../../device/mediatek/mt6737-common/patches/HIDL-sensors-fallback-to-API_1_0.patch
+patch -p1 < ../../device/mediatek/mt6737-common/Error-message-suppress-option.patch
+patch -p1 < ../../device/mediatek/mt6737-common/Workaround-mtk-camera-blob.patch
+patch -p1 < ../../device/mediatek/mt6737-common/Disable-vndk-for-the-time-being.patch
 cd ../..
